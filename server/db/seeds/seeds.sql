7244cf39495eea69ebd080aebe06e916f877391f
Justin Huang <justin.2260@gmail.com>
2022-03-17 01:31:03 -0400

    fix error

--- server/db/seeds/seeds.sql
+++ server/db/seeds/seeds.sql
@@ -1,9 +1,9 @@
 -- sample users
-INSERT INTO users (username, email, password) VALUES ("mudhippo", "hippo@cat.com", "password");
-INSERT INTO users (username, email, password) VALUES ("pinkthing", "flamingo@cat.com", "password");
-INSERT INTO users (username, email, password) VALUES ("fastbirb", "ostrich@cat.com", "password");
-INSERT INTO users (username, email, password) VALUES ("gator", "allidile@cat.com", "password");
-INSERT INTO users (username, email, password) VALUES ("dile", "crocogator@cat.com", "password");
+INSERT INTO users (username, email, password) VALUES ('mudhippo', 'hippo@cat.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('pinkthing', 'flamingo@cat.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('fastbirb', 'ostrich@cat.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('gator', 'allidile@cat.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('dile', 'crocogator@cat.com', 'password');
 
 
 -- sample scores for standard game
@@ -19,7 +19,7 @@ INSERT INTO standard_scores (score, user_id) VALUES (91, 5);
 
 -- sample scores for number game
 INSERT INTO number_scores (score, user_id) VALUES (8, 1);
-INSERT INTO number_scores (score, user_id) VALUES (671 1);
+INSERT INTO number_scores (score, user_id) VALUES (67, 1);
 INSERT INTO number_scores (score, user_id) VALUES (66, 1);
 INSERT INTO number_scores (score, user_id) VALUES (31, 2);
 INSERT INTO number_scores (score, user_id) VALUES (70, 3);
