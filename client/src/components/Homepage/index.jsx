0a6b71aecd3a8408bce2f132b91c2b3015f3b8f3
Justin Huang <justin.2260@gmail.com>
2022-03-26 15:45:20 -0400

    initial blueprints for components to be used in game

---
+++ client/src/components/Homepage/index.jsx
@@ -0,0 +1,15 @@
+import "./index.scss";
+
+const Homepage = () => {
+  return(
+    <>
+    <h2>HI</h2>
+    <div className="game-buttons">
+      <button>Normal Tap Game</button>
+      <button>Number Tap Game</button>
+    </div>
+    </>
+  )
+}
+
+export default Homepage;
