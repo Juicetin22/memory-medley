8a68cb955ab7dd5952a02dfcd56d2984fd27ae70
Justin Huang <justin.2260@gmail.com>
2022-03-26 15:46:45 -0400

    update name of a table

--- server/db/seeds/seeds.sql
+++ server/db/seeds/seeds.sql
@@ -7,15 +7,15 @@ INSERT INTO users (username, email, password) VALUES ('dile', 'crocogator@cat.co
 
 
 -- sample scores for standard game
-INSERT INTO standard_scores (score, user_id) VALUES (10, 1);
-INSERT INTO standard_scores (score, user_id) VALUES (69, 1);
-INSERT INTO standard_scores (score, user_id) VALUES (98, 1);
-INSERT INTO standard_scores (score, user_id) VALUES (22, 2);
-INSERT INTO standard_scores (score, user_id) VALUES (33, 3);
-INSERT INTO standard_scores (score, user_id) VALUES (44, 4);
-INSERT INTO standard_scores (score, user_id) VALUES (100, 4);
-INSERT INTO standard_scores (score, user_id) VALUES (22, 5);
-INSERT INTO standard_scores (score, user_id) VALUES (91, 5);
+INSERT INTO simple_scores (score, user_id) VALUES (10, 1);
+INSERT INTO simple_scores (score, user_id) VALUES (69, 1);
+INSERT INTO simple_scores (score, user_id) VALUES (98, 1);
+INSERT INTO simple_scores (score, user_id) VALUES (22, 2);
+INSERT INTO simple_scores (score, user_id) VALUES (33, 3);
+INSERT INTO simple_scores (score, user_id) VALUES (44, 4);
+INSERT INTO simple_scores (score, user_id) VALUES (100, 4);
+INSERT INTO simple_scores (score, user_id) VALUES (22, 5);
+INSERT INTO simple_scores (score, user_id) VALUES (91, 5);
 
 -- sample scores for number game
 INSERT INTO number_scores (score, user_id) VALUES (8, 1);
