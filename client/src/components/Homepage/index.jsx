83831ab4d41a66bf1db76d0e3e919991333eed20
Justin Huang <justin.2260@gmail.com>
2022-03-26 20:45:19 -0400

    initial how to play button

--- client/src/components/Homepage/index.jsx
+++ client/src/components/Homepage/index.jsx
@@ -1,13 +1,17 @@
 import "./index.scss";
+import { Button } from 'react-bootstrap'
 
 const Homepage = () => {
   return(
     <>
-    <h2>HI</h2>
-    <div className="game-buttons">
-      <button>Normal Tap Game</button>
-      <button>Number Tap Game</button>
-    </div>
+      <h2 className="opening-words">Welcome to Tap Games!</h2>
+      <p>Tap on either button below to start playing!</p>
+      
+      <div className="game-buttons">
+        <Button className="classic-button">Classic Tap Game</Button>
+        <Button variant="danger" className="number-button">Number Tap Game</Button>
+      </div>
+      <Button variant="outline-secondary" className="how-to-play">How to play</Button>
     </>
   )
 }
