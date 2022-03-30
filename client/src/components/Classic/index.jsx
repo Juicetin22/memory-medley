92e981a76d17ba67d8ad5111e980c4636c901254
Justin Huang <justin.2260@gmail.com>
2022-03-30 11:26:10 -0400

    implement target that users need to tap on, also implemented the repositioning and colour change of the target upon clicking

--- client/src/components/Classic/index.jsx
+++ client/src/components/Classic/index.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import GameBoard from "../GameBoard";
 import "./index.scss";
 
 const Classic = () => {
+  const [position, setPosition] = useState({left: "50%", top: "50%", "background-color": "black"});
+
+  const random = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const reposition = () => {
+    setPosition(prev => ({
+      ...prev, 
+      left: `${random(15, 85)}%`, 
+      top: `${random(20, 80)}%`, 
+      "background-color": `rgb(${random(0, 250)}, ${random(0, 250)}, ${random(0, 250)}`
+    }));
+  }
+  
   return (
     <div>
-      Classic
+      <GameBoard />
+      <div className="classic-piece" style={position} onClick={reposition}></div>
     </div>
   )
 }
