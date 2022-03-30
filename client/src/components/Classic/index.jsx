24abb3c501891049077c3920cd13636df22740fa
Justin Huang <justin.2260@gmail.com>
2022-03-30 18:28:32 -0400

    implement back button, and new game button, also disable tapping once time limit is reached

--- client/src/components/Classic/index.jsx
+++ client/src/components/Classic/index.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import GameBoard from "../GameBoard";
 import "./index.scss";
+import classNames from "classnames";
+import { Link } from "react-router-dom";
 
 const Classic = () => {
   const [position, setPosition] = useState({left: "50%", top: "50%", "background-color": "black"});
   const [score, setScore] = useState(0);
-  const [time, setTime] = useState(100);
+  const [time, setTime] = useState(10);
+  const [start, setStart] = useState(false);
+  const [end, setEnd] = useState(false);
 
   const random = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -20,11 +24,23 @@ const Classic = () => {
     }));
 
     setScore(prev => prev + 1);
+    
+    if (!start) {
+      setStart(true);
+    }
+  }
+
+  const reset = () => {
+    setPosition(prev => ({...prev, left: "50%", top: "50%", "background-color": "black"}));
+    setScore(0);
+    setTime(10);
+    setStart(false);
+    setEnd(false);
   }
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (time > 0) {
+      if (start && time > 0) {
         setTime(prev => prev - 1)
       }
     }, 1000);
@@ -33,17 +49,29 @@ const Classic = () => {
     return () => {
       clearInterval(interval)
     }
+  }, [time, start]);
+
+  useEffect(() => {
+    if (time === 0) {
+      setEnd(true);
+    }
   }, [time]);
+
+  const classicPiece = classNames("classic-piece", { "end": end });
   
   return (
     <div>
-      <h3 className="classic-game-header">Classic Tap Game</h3>
+      <div className="top">
+        <Link to="/" className="link"><button className="back-button">← Back</button></Link>
+        <h3 className="classic-game-header">Classic Tap Game</h3>
+        <button onClick={reset} className="new-game">New Game</button>
+      </div>
       <div className="time-score">
         <p>Time Remaining: {time}</p>
         <p><strong>Score: {score}</strong></p>
       </div>
       <GameBoard />
-      <div className="classic-piece" style={position} onClick={reposition}></div>
+      <div className={classicPiece} style={position} onClick={reposition}></div>
     </div>
   )
 }
