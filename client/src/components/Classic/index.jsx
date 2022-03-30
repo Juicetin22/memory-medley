91c86dd2633ec14264104e172617ad01415c4343
Justin Huang <justin.2260@gmail.com>
2022-03-30 11:35:29 -0400

    initial setup of score state

--- client/src/components/Classic/index.jsx
+++ client/src/components/Classic/index.jsx
@@ -4,6 +4,7 @@ import "./index.scss";
 
 const Classic = () => {
   const [position, setPosition] = useState({left: "50%", top: "50%", "background-color": "black"});
+  const [score, setScore] = useState(0);
 
   const random = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -16,12 +17,15 @@ const Classic = () => {
       top: `${random(20, 80)}%`, 
       "background-color": `rgb(${random(0, 250)}, ${random(0, 250)}, ${random(0, 250)}`
     }));
+
+    setScore(prev => prev + 1);
   }
   
   return (
     <div>
       <GameBoard />
       <div className="classic-piece" style={position} onClick={reposition}></div>
+      <p>Score: {score}</p>
     </div>
   )
 }
