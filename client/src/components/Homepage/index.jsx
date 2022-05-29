a19a25c79dae94c26e2910604a02d131a5081ea0
Justin Huang <justin.2260@gmail.com>
2022-05-29 19:06:36 -0400

    initial formatting of card on homepage

--- client/src/components/Homepage/index.jsx
+++ client/src/components/Homepage/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import "./index.scss";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
-    <div>
-      hi
-    </div>
+    <>
+      <Card className="home-card">
+        <Card.Body>
+          <h2 className="title">Welcome to Memory Medley!</h2>
+          <p>Please choose one of the following game modes to begin ☺️</p>
+          <div>
+         
+          </div>
+        </Card.Body>
+      </Card>
+    </>
   )
 }
 
