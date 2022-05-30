172100bd99a4cbfd5614978ddacb4cdee19d9b81
Justin Huang <justin.2260@gmail.com>
2022-05-30 01:47:21 -0400

    add route to memorage game mode

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Homepage from "./components/Homepage";
 import TapGame from "./components/Tap-Game/Main";
 import Classic from "./components/Tap-Game/Classic";
 import Number from "./components/Tap-Game/Number";
+import Memorage from "./components/Memorage/Main";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path={"/tap-game/"} element={<TapGame />} />
         <Route path={"/tap-game/classic"} element={<Classic />} />
         <Route path={"/tap-game/number"} element={<Number />} />
+        <Route path={"/memorage"} element={<Memorage />} />
       </Routes>
     </div>
   );
