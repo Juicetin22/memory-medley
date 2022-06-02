66f7851a72d6f5cf30576dba55f62f0849a81688
Justin Huang <justin.2260@gmail.com>
2022-06-02 17:13:44 -0400

    change import path to reflect files movement changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
-import TapGame from "./components/Tap-Game/Main";
+import TapGame from "./components/Tap-Game";
 import Classic from "./components/Tap-Game/Classic";
 import Number from "./components/Tap-Game/Number";
-import Memorage from "./components/Memorage/Main";
+import Memorage from "./components/Memorage";
 
 function App() {
   return (
