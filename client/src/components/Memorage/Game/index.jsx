13dbfa22634acbc3ac43c60bc6957caac7219307
Justin Huang <justin.2260@gmail.com>
2022-06-02 21:46:47 -0400

    test for displaying image on page

--- client/src/components/Memorage/Game/index.jsx
+++ client/src/components/Memorage/Game/index.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import "./index.scss";
+
+const images = [
+  { id: 1, src: "/images/image-1.png" }
+];
 
 const MemorageGame = () => {
+  
+  const [image, setImage] = useState({});
+
+  const random = () => {
+
+  }
+
   return (
     <>
-      HI
+      <div className="image-holder">
+        <img src={images[0].src} />
+      </div>
     </>
   )
 }
