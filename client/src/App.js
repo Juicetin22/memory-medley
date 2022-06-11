954dacccc35a266e1d03289cd53cfa06ed733975
Justin Huang <justin.2260@gmail.com>
2022-06-11 00:20:20 -0400

    import memory games into App.js with corresponding component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
+import MemoryGames from "./components/Memory-Games";
+import Matching from "./components/Memory-Games/Matching";
 import TapGame from "./components/Tap-Game";
 import Classic from "./components/Tap-Game/Classic";
 import Number from "./components/Tap-Game/Number";
@@ -13,7 +15,9 @@ function App() {
     <div className="App">
        <Routes>
         <Route path={"/"} element={<Homepage />} />
-        <Route path={"/tap-game/"} element={<TapGame />} />
+        <Route path={"/memory-games"} element={<MemoryGames />} />
+        <Route path={"/memory-games/matching-cards"} element={<Matching />} />
+        <Route path={"/tap-game"} element={<TapGame />} />
         <Route path={"/tap-game/classic"} element={<Classic />} />
         <Route path={"/tap-game/number"} element={<Number />} />
         <Route path={"/memorage"} element={<Memorage />} />
